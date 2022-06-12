67e25a2126e4ccc080e79f024e9e86ad4862f5e8
Leroys99 <fanhag96@gmail.com>
2022-06-12 15:30:59 +0300

    menu header jump fix, textarea padding

--- menu.js
+++ menu.js
@@ -1,11 +1,8 @@
 const btn__burger = document.querySelector("#btn-burger");
-const btn__close = document.querySelector("#btn-close");
 const header = document.querySelector("#header");
 
 btn__burger.addEventListener("click", function () {
-  header.classList.add("menu-open");
-
-  btn__close.addEventListener("click", function () {
-    header.classList.replace("menu-open", "header");
-  });
+  header.classList.toggle("menu-open");
+  btn__burger.classList.toggle("btn-close");
 });
+
